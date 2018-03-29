fa2442d657f059ff9d64f529414a6b786eed85ec
Jose Ramirez <shadow661199@gmail.com>
2018-03-28 21:45:50 -0700

    updated portfolio but still incomplete

--- portfolio/Javascript/script.js
+++ portfolio/Javascript/script.js
@@ -1,12 +1,20 @@
-function changeBackgroundColor()
-{
-window.document.bgColor="black";
-//window.document.fgColor="rgba(0, 255, 213, 0.3)";
-document.getElementById('red').style.color="red";
-document.body.style.color="red";
-}
-
-// function color(){
-// document.getElementById('red').style.color="red";
+// function changeBackgroundColor()
+// {
+// window.document.bgColor="black";
+// //window.document.fgColor="rgba(0, 255, 213, 0.3)";
+// //document.getElementsByClassName('change').style.backgroundColor="red";
+// //document.body.style.color="red";
+// //toggleColor();
 // }
+
+// toggleColor();
+
+// function toggleColor(){
+
+//     document.body.classList.toggle('changeColor');
+// }
+
+// // function color(){
+// // document.getElementById('red').style.color="red";
+// // }
 
