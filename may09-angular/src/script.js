ce948d0530344deaa4ebf20ca1339179a10a941b
Jose Ramirez <shadow661199@gmail.com>
2018-05-20 16:21:46 -0700

    may09 assignment but in angular

---
+++ may09-angular/src/script.js
@@ -0,0 +1,14 @@
+var value = 5;
+
+
+
+
+document.getElementById('test').innerHTML = "I am A house" +' '+ value;
+
+
+if(value > 10){
+document.getElementById('warn').innerHTML ="Damn";
+}
+
+const colors =['red','green','yellow','blue'];
+
