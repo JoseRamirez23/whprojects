fee85ec321a9bb60e1c3d0a9c900da43624e1020
Jose Ramirez <shadow661199@gmail.com>
2018-06-01 14:46:04 -0700

    understand directives

--- components/src/app/server/server.components.ts
+++ components/src/app/server/server.components.ts
@@ -4,17 +4,40 @@ import { Component } from "@angular/core";
 @Component({
     selector:'app-server',
     //external template
-    templateUrl:'./server.component.html'
+    templateUrl:'./server.component.html',
+    styles:[`
+        .online{
+
+            color:white;
+        }
+
+        .standard{
+
+            font-size:24px;
+        }
+    
+    `]
+
+
 
     //internal template
     //template:`<app-server></app-server>`,
 })
 export class ServerComponent{
     serverId:number = 10;
-    serverStatus:string='offline';
+    serverStatus:string;
+
+    constructor(){
+        this.serverStatus= Math.random( )> 0.5 ? 'Online' : 'Offline';
+    }
 
     getServerStatus(){
 
         return this.serverStatus;
+    }
+
+    getBackgroundColor(){
+
+        return this.serverStatus === 'Online' ? 'green': 'red';
     }
 }
--- components/src/app/servers/servers.component.ts
+++ components/src/app/servers/servers.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { setTimeout } from 'timers';
 
 @Component({
   //selector: 'app-servers',//this is an element selector
@@ -8,14 +7,38 @@ import { setTimeout } from 'timers';
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
+
   allowNewServer:boolean=false;
-  constructor() { 
+  serverCreationStatus:string='Now server was created';
+  serverName:string;
 
-    setTimeout(()=>{this.allowNewServer=true;},2000);
+  serverCreated:boolean=false;
+  servers = ['testserver','testserver2'];
+  constructor() {
+
+    setTimeout(() => {
+      
+      
+      this.allowNewServer = true;
+    })
     
-  }
-
+   }
+    
   ngOnInit() {
   }
+
+  onCreateServer(){
+    this.serverCreated=true;
+    this.serverCreationStatus='New Server' + this.serverName;
+    this.servers.push(this.serverName);
+  }
+
+  //this is connected to the $event in the html file
+//you can change it to any to Event because you know it's an event
+    onUpdateServerName(event:Event ){
+      console.log(event);
+    this.serverName = (<HTMLInputElement>event.target).value;
+
+    }
 
 }
